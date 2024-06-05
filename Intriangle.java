45beed01568611360a1ebf391ca367cb27b3e705
omi-cody <omshankarsah4548@gmail.com>
2024-06-05 10:12:15 +0545

    increasing-triangle-pattern

---
+++ Intriangle.java
@@ -0,0 +1,14 @@
+public class Intriangle {
+    public Intriangle(){
+        int n=6;
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void main(String[] args) {
+        new Intriangle();
+    }
+}
