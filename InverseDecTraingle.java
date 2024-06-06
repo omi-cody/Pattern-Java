d3e63eb2fb9a6c7fae03da9af9d67f4bbba0b9ad
omi-cody <omshankarsah4548@gmail.com>
2024-06-06 17:51:25 +0545

    Update-Pattern

--- InverseDecTraingle.java
+++ InverseDecTraingle.java
@@ -10,6 +10,7 @@ public class InverseDecTraingle {
             for (int j = i; j <= n; j++) {
                 System.out.print("* ");
             }
+            
             System.out.println(" ");
 
         }
