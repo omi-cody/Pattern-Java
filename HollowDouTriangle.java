d156065129d10e6e98b8f015c7019a9fc7a596d9
omi-cody <omshankarsah4548@gmail.com>
2024-06-06 18:14:40 +0545

    Hollow-Double-Triangle-Pattern

---
+++ HollowDouTriangle.java
@@ -0,0 +1,34 @@
+public class HollowDouTriangle {
+    public HollowDouTriangle(){
+        int n = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j < i; j++) {
+                if (i==n || j==1) {
+                    System.out.print("* ");
+                    
+                } else {
+                    System.out.print("  ");
+                    
+                }
+            }
+            for (int j = 1; j <=i; j++) {
+                if (i==n || j==i) {
+                    System.out.print("* ");
+                    
+                } else {
+                    System.out.print("  ");
+                    
+                }
+            }
+
+            System.out.println(" ");
+
+        }
+    }
+    public static void main(String[] args) {
+        new HollowDouTriangle();
+    }
+}
